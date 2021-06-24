11856362bdaa082a01daff3426fa8abdf7514b70
Mohamed jinas <j@live.mv>
2021-06-24 13:38:20 +0500

    switch sides

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,14 +5,16 @@
         @csrf
 
         <div x-data="{ isOpen: false }" class="h-screen flex overflow-hidden">
-            <x-main>
-                @include('_editor')
-            </x-main>
-
             <x-nav>
                 <x-nav-item label="Save" type="submit" icon="heroicon-o-save" />
                 <x-nav-item label="Reset" type="reset" icon="heroicon-o-ban" />
             </x-nav>
+            
+            <x-main>
+                @include('_editor')
+            </x-main>
+
+           
         </div>
     </form>
 @stop
