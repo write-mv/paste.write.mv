2a739a1b53f5f16db0bf5c44ff1fda17809d6871
Mohamed jinas <j@live.mv>
2021-06-24 13:39:13 +0500

    switch sidebar side

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -5,15 +5,17 @@
         @csrf
 
         <div x-data="{ isOpen: false }" class="h-screen flex overflow-hidden">
-            <x-main>
-                @include('_editor')
-            </x-main>
-
             <x-nav>
                 <x-nav-item label="Save" type="submit" icon="heroicon-o-save" />
                 <x-nav-item label="Reset" type="reset" icon="heroicon-o-ban" />
                 <x-nav-item label="Back" :link="route('show', $paste->hash)" icon="heroicon-o-arrow-circle-left" />
             </x-nav>
+            
+            <x-main>
+                @include('_editor')
+            </x-main>
+
+         
         </div>
     </form>
 @stop
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,13 +2,6 @@
 
 @section('content')
     <div x-data="{ isOpen: false }" class="h-screen flex overflow-hidden">
-        <x-main>
-            <div
-                class="h-full text-right faseyha text-gray-700 dark:text-white text-md prettyprint linenums selectable" data-line-numbers="true" dir="rtl"
-            >{!!$paste->content !!}
-        </div>
-        </x-main>
-
         <x-nav>
             <x-nav-item label="New" :link="route('home')" icon="heroicon-o-document-add" />
             <x-nav-item
@@ -21,5 +14,14 @@
             <x-nav-item label="Fork" :link="route('edit', $paste->hash)" icon="heroicon-o-document-duplicate" />
             <x-nav-item label="Raw" :link="route('raw', $paste->hash)" icon="heroicon-o-external-link" blank />
         </x-nav>
+       
+        <x-main>
+            <div
+                class="h-full text-right faseyha text-gray-700 dark:text-white text-md prettyprint linenums selectable" data-line-numbers="true" dir="rtl"
+            >{!!$paste->content !!}
+        </div>
+        </x-main>
+
+       
     </div>
 @stop
