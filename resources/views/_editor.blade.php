3cc86239269ea4f737c55eb8a086e6fed08ebf15
Mohamed jinas <j@live.mv>
2021-06-25 06:57:19 +0500

    enable font in editor

--- resources/views/_editor.blade.php
+++ resources/views/_editor.blade.php
@@ -1,7 +1,7 @@
 
 <div class="h-full font-mono text-sm">
     <textarea
-        name="content" class="dark:bg-dark text-gray-900 dark:text-white w-full h-full resize-none outline-none thaana-keyboard"
+        name="content" class="dark:bg-dark text-gray-900 dark:text-white w-full h-full resize-none outline-none faseyha thaana-keyboard"
         dir="rtl"
     >{{ optional($paste ?? null)->content }}</textarea>
 </div>
